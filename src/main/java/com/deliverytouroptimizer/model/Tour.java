7bdc28c67f23c81d110648ce66b4b2975e2a7821
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 21:54:06 +0100

    DTO-19 Create Tour entity

---
+++ src/main/java/com/deliverytouroptimizer/model/Tour.java
@@ -0,0 +1,38 @@
+package com.deliverytouroptimizer.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tour {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Tour date is required")
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
+    @OneToMany(mappedBy = "tour", cascade = CascadeType.ALL)
+    @OrderColumn(name = "delivery_order")
+    private List<Delivery> deliveries;
+}
