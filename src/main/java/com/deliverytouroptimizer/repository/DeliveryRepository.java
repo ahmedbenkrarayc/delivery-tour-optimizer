b23eeaec69952abda0018785f5772d43002e77a1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 23:52:21 +0100

    DTO-25 Created custom queries findByStatus, findByWarehouseId in DeliveryRepository

--- src/main/java/com/deliverytouroptimizer/repository/DeliveryRepository.java
+++ src/main/java/com/deliverytouroptimizer/repository/DeliveryRepository.java
@@ -1,4 +1,11 @@
 package com.deliverytouroptimizer.repository;
 
+import com.deliverytouroptimizer.model.Delivery;
+import com.deliverytouroptimizer.model.enums.DeliveryStatus;
+
+import java.util.List;
+
 public interface DeliveryRepository {
+    List<Delivery> findByStatus(DeliveryStatus status);
+    List<Delivery> findByWarehouseId(Long warehouseId);
 }
