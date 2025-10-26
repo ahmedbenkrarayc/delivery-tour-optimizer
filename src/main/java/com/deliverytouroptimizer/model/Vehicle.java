528b0561084a189cb0814af996644ad920350be3
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 21:52:42 +0100

    DTO-19 Create Vehicle entity

---
+++ src/main/java/com/deliverytouroptimizer/model/Vehicle.java
@@ -0,0 +1,40 @@
+package com.deliverytouroptimizer.model;
+
+import com.deliverytouroptimizer.model.enums.VehicleType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Vehicle type is required")
+    @Enumerated(EnumType.STRING)
+    private VehicleType type;
+
+    @Min(value = 0, message = "Max weight must be zero or positive")
+    private double maxWeight;
+
+    @Min(value = 0, message = "Max volume must be zero or positive")
+    private double maxVolume;
+
+    @Min(value = 1, message = "Max deliveries must be at least 1")
+    private int maxDeliveries;
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL)
+    private List<Tour> tours;
+}
