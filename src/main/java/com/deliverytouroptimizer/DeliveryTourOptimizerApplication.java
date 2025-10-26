b66afdf78cba90465efd1c8b91467c868e11554e
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 21:28:36 +0100

    DTO-8 Initialize the project

---
+++ src/main/java/com/deliverytouroptimizer/DeliveryTourOptimizerApplication.java
@@ -0,0 +1,21 @@
+package com.deliverytouroptimizer;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTourOptimizerApplication {
+
+	public static void main(String[] args) {
+		loadEnv();
+		SpringApplication.run(DeliveryTourOptimizerApplication.class, args);
+	}
+
+	private static void loadEnv(){
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+	}
+}
---
+++ src/test/java/com/deliverytouroptimizer/DeliveryTourOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytouroptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTourOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
