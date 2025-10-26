97a8a9c15b83f6b26f3b3576de771adb19a3c6e4
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 21:50:24 +0100

    DTO-19 Create Warehouse entity

---
+++ src/main/java/com/deliverytouroptimizer/model/Warehouse.java
@@ -0,0 +1,39 @@
+package com.deliverytouroptimizer.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Warehouse name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Warehouse address cannot be blank")
+    private String address;
+
+    @NotNull(message = "Warehouse opening time is required")
+    private LocalTime openingTime;
+
+    @NotNull(message = "Warehouse closing time is required")
+    private LocalTime closingTime;
+
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL)
+    private List<Tour> tours;
+}
