f65b602da0eb6c01631e8047a18a1731e3df01b2
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 23:42:03 +0100

    DTO-24 Created WarehouseRepository

---
+++ src/main/java/com/deliverytouroptimizer/repository/WarehouseRepository.java
@@ -0,0 +1,7 @@
+package com.deliverytouroptimizer.repository;
+
+import com.deliverytouroptimizer.model.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+}
