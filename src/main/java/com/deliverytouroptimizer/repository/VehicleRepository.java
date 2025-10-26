42fd3c463683a2db9f0ecd310477c0cea685daa6
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-26 23:53:11 +0100

    DTO-25 Created custom query findByType in VehicleRepository

--- src/main/java/com/deliverytouroptimizer/repository/VehicleRepository.java
+++ src/main/java/com/deliverytouroptimizer/repository/VehicleRepository.java
@@ -1,7 +1,11 @@
 package com.deliverytouroptimizer.repository;
 
 import com.deliverytouroptimizer.model.Vehicle;
+import com.deliverytouroptimizer.model.enums.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findByType(VehicleType type);
 }
