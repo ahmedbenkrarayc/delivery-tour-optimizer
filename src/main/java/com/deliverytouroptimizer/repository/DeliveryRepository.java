8601aacf73639a58ffcaa6d352593802c34e2c88
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-27 11:28:36 +0100

    DTO-24 Fixed DeliveryRepository to extend JpaRepository

--- src/main/java/com/deliverytouroptimizer/repository/DeliveryRepository.java
+++ src/main/java/com/deliverytouroptimizer/repository/DeliveryRepository.java
@@ -1,11 +1,13 @@
 package com.deliverytouroptimizer.repository;
 
 import com.deliverytouroptimizer.model.Delivery;
+import com.deliverytouroptimizer.model.Tour;
 import com.deliverytouroptimizer.model.enums.DeliveryStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface DeliveryRepository {
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
     List<Delivery> findByStatus(DeliveryStatus status);
     List<Delivery> findByWarehouseId(Long warehouseId);
 }
