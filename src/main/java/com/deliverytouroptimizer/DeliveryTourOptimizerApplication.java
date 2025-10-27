4ed12101315d2ec3de219c7da47672e64a734a48
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-10-27 11:34:04 +0100

    DTO-11 Imported applicationContext

--- src/main/java/com/deliverytouroptimizer/DeliveryTourOptimizerApplication.java
+++ src/main/java/com/deliverytouroptimizer/DeliveryTourOptimizerApplication.java
@@ -3,8 +3,10 @@ package com.deliverytouroptimizer;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class DeliveryTourOptimizerApplication {
 
 	public static void main(String[] args) {
